4be5791a23c8edeec74b7a35c08be411fa38a796
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-10-01 17:10:59 +0400

    initial commit

---
+++ print.py
@@ -0,0 +1,10 @@
+print ("Hello World ")
+print ("My name is Dhruthi ")
+
+#printing I love soccer 
+print ("I love soccer")
+
+#Performing and printing a simple calculation
+
+print ("The result of 3 + 5 is :" , 3 + 5 )
+
