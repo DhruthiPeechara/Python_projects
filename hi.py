75a02aa34e24f1a0bffbd8d256607a7177eb25db
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-10-01 17:12:03 +0400

    initial commit

---
+++ hi.py
@@ -0,0 +1,9 @@
+#variable 
+
+i = 3 
+x = 5 
+
+#printing the product 
+
+print ("The product is : " 3*5 )
+
---
+++ project.py
@@ -0,0 +1,14 @@
+# printing a sentence 
+print ("My name is Dhruthi ")
+
+# printing a number 
+print (4)
+
+#printing another number 
+print (5)
+
+#Printing sum 
+print ("The sum of 3,5 is :" , 3+5)
+
+
+
