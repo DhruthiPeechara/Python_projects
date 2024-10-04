f5f2ef801e5fec1cce9a75d53d56a6a71c82bba5
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-10-04 19:30:48 +0400

    initial commit

---
+++ arthematic.py
@@ -0,0 +1,13 @@
+#Storing values 
+
+tree1 = 98
+tree2 = 94 
+tree3 = 41
+tree4 = 95
+tree5 = 11 
+
+sum = tree1+tree2+tree3+tree4+tree5 
+
+print ("The sum of all the trees height is:" , sum )
+print ("The average of the trees height is " , sum/3)
+
