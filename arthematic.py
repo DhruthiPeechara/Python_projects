e376faab3d513d2bffeb93acaabbc3b2915ec215
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-10-11 18:55:41 +0400

    initial commit

--- arthematic.py
+++ arthematic.py
@@ -11,3 +11,12 @@ sum = tree1+tree2+tree3+tree4+tree5
 print ("The sum of all the trees height is:" , sum )
 print ("The average of the trees height is " , sum/3)
 
+num = 3 
+if num > 0 :
+    print(num, "is a positive number ")
+
+    num = -1 
+if num > 0 :
+    print(num, "is a positive number ")
+
+    
---
+++ logic_operators.py
@@ -0,0 +1,9 @@
+a = 23
+b = 34 
+c = 56 
+
+if (c>a) :
+    print ("C is greater ")
+else :
+    print ("A is greater ")
+    
