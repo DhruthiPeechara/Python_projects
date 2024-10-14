94fe2aa39c46cbe848f249d9094efcca01ef83cb
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-10-14 19:06:38 +0400

    initial commit

---
+++ nesting.py
@@ -0,0 +1,17 @@
+
+e=input ("Do you have a medical reason Y or N: ")
+a =int(input ("What is your attendance percentage "))
+
+if (e=="Medical cause ") :
+    print ("Eligible to write the exam ")
+
+if (a >=75) :
+    print ("Eligible to write the exam ")
+
+else:
+    print ("Not eligible to write the exam " )
+
+
+
+
+
