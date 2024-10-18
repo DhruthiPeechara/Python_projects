22d4f569ad08893631fc93415c1b5b475d366f94
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-10-18 19:18:02 +0400

    initial commit

---
+++ loops_3.py
@@ -0,0 +1,9 @@
+#input number 
+n = int (input("Enetr the value of n: "))
+
+#Print numbers from n to 1 
+print ("numbers from {0} to {1} are :" .format (n,1) )
+
+
+#loop to print numbers 
+for i in range (n)
