0eb89c7a0122e9df6b95392b7054da4af7d5b650
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-10-18 18:59:38 +0400

    initial commit

---
+++ loops_2.py
@@ -0,0 +1,8 @@
+string = input ("Please enter your own string:")
+
+string2 = ('')
+for i in string : 
+    string2 = i + string2 
+
+    print ("/n The original String = " , string )
+    print ("The Reversed String = " , string2)
