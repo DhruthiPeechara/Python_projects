49b01f0c0e838c323ed9b55b7ecb7cb7ee3fd3a0
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-10-21 18:49:03 +0400

    initial commit

---
+++ while_loops.py
@@ -0,0 +1,15 @@
+#Input the data from the user 
+
+n = int(input("Enter the value of terms:"))
+
+sum = 0 
+i = 1 
+
+while i<=n: 
+    sum = sum+1 
+
+    i = i+i 
+
+    print ("/nsum =" , sum )
+
+    
