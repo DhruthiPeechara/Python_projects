18164ca67b266912996af18fa588a8003e465173
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-10-25 19:13:23 +0400

    initial commit

---
+++ nested_loops_2.py
@@ -0,0 +1,8 @@
+lower = int(input("Enetr a upper range"))
+upper = int(input("Enter a lower range "))
+
+print ("prime numbers between", lower,"and",upper,"are")
+
+for num in range (lower,upper+1):
+    if num>1: 
+        for i in range 
