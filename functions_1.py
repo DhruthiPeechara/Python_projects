06609d82ccf1075ba17e4dc9591e525706818cd8
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-11-04 19:24:16 +0400

    initial commit

---
+++ functions_1.py
@@ -0,0 +1,10 @@
+def weather_conditions():
+
+    print("the weathe is pleasant in, autumn ")
+    print ("The weather is the same in , spring ")
+
+    spring= "autumn"
+    autumn= spring
+
+weather_conditions()
+    
