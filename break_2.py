2bb99639905a177f1c864792fd42b8a5d392a1ab
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-11-11 19:41:20 +0400

    initial commit

---
+++ break_2.py
@@ -0,0 +1,16 @@
+for x in range(10):
+    if x % 20 == 0:
+        print("Twist")
+
+    elif x % 15  == 0:
+     pass 
+
+    elif x % 5 == 0:
+     print("fizz")
+
+    elif x %1 == 0:
+     print("bomb")
+
+    else :
+      print(x)
+     
