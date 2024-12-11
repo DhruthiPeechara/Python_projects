42cad9fc449781cbadcabfffc8c8e4e322b350c9
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-12-11 20:57:49 +0400

    initial commit

---
+++ lists_2.py
@@ -0,0 +1,9 @@
+def match_words(words):
+    ctr = 0 
+    lst = []
+    for word in words:
+            if len (word)> 1 and word [0]== word[-1]:
+                    ctrl += 1
+                    lst.ppend (word)
+
+    print 
