b9a6816f0a16ee25ae6442904f769762b2473656
DhruthiPeechara <dhruthi.peechara@gmail.com>
2024-12-30 11:45:15 +0400

    initial commit

---
+++ polymorphism.py
@@ -0,0 +1,10 @@
+def move (self):
+    class Human(Animal):
+        def move(self):
+            print("I can walk and run ")
+
+class snake(Animal):
+        def move(self):
+             print("I can crawl ")
+
+class
