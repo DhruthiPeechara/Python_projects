9f90617811d4eb0c4d6c760647b65593f277942c
DhruthiPeechara <dhruthi.peechara@gmail.com>
2025-07-28 13:27:14 +0400

    INITIAL COMMIT

---
+++ __pycache__/mydatabase.db/operators.sql
@@ -0,0 +1,17 @@
+CREATE TABLE if NOT EXIST student (
+
+    NAME TEXT NOT NULL, 
+    ADRESS TEXT, 
+    PHONE TEXT, 
+
+);
+
+INSERT INTO STUDENT (ROLL_N0, NAME, ADDRESS, PHONE, AGE ) VALUES
+('1', 'DIYA', 'DELHI', '***********', 18),
+('2', 'TINA', 'DUBAI', '***********', 13),
+('3', 'SANDY', 'LONDON', '***********', 28),
+('1', 'DIYA', 'DELHI', '***********', 18),
+
+
+
+
---
+++ __pycache__/mydatabase.db/table_2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS salesman (
+    salesman_id TEXT PRIMARY KEY 
+    name TEXT , 
+    city TEXT , 
+    Commision REAL 
+); 
+
+INSERT INTO salesman ()
+
+
