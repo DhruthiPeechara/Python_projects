0fa94bddb23b7a7691b9534e6dd7b345695c6de6
DhruthiPeechara <dhruthi.peechara@gmail.com>
2025-08-12 10:13:03 +0400

    INITIAL COMMIT

---
+++ __pycache__/mydatabase.db/sqlite.sql
@@ -0,0 +1,16 @@
+[]
+    from google.colad import FILESTREAM
+    file = files.upload()
+
+[]
+
+    import sqlite3
+
+    database = 'database.sqlite'
+     conn = sqlite.connect(database)
+     print('Opened data sucessfully')
+
+     import pandas as pd
+    tables = pd.read_sql("""SELECT*
+    )
+
